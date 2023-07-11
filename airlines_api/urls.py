b61efc02faf03da9e853a62b5f96aa66b2730499
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-07-11 12:35:15 +0300

    Added path for total-info

--- airlines_api/urls.py
+++ airlines_api/urls.py
@@ -3,6 +3,8 @@ from rest_framework import routers
 
 from airlines_api.views import (
     AircraftViewSet,
+    AircraftList,
+    TotalInfo,
 
 )
 
@@ -12,6 +14,9 @@ router.register("aircraft", AircraftViewSet, basename="aircraft")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("info/", AircraftList.as_view(), name="info-list"),
+    path("total/", TotalInfo.as_view(), name="total-info"),
+
 ]
 
 app_name = "aircraft"
